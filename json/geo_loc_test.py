61fbb687ce2f87834fc4249d565e918f4b00bfd8
Timo K. <elveli@gmail.com>
2024-09-25 21:25:51 -0700

    fixed getting lat data

--- json/geo_loc_test.py
+++ json/geo_loc_test.py
@@ -8,28 +8,41 @@ printer = PrettyPrinter()
 
 key = os.getenv('OPENCAGE_KEY')
 #print(key)
+
+if not key:
+    print("API key is missing. Please set the OPENCAGE_KEY environment variable.")
+    exit(1)
+
 #url = "http://api.opencagedata.com/geocode/v1/json?key=key&q=52.3877830%2C+9.7334394&pretty=1&no_annotations=1"
 
-url1="http://api.opencagedata.com/geocode/v1/json?key="
-url2=key
-url3="&q=52.3877830%2C+9.7334394&pretty=1&no_annotations=1"
-
-url = url1 + url2 + url3
-print(url)
+url = f"http://api.opencagedata.com/geocode/v1/json?key={key}&q=52.3877830%2C+9.7334394&pretty=1&no_annotations=1"
 
 
 response = requests.get(url)
 data = json.loads(response.text)
 
-# name = data['name']
-# premiered = data['premiered']
-# summary = data['summary']
-# network = data['network']['name']
 
+# Extract the location bounds from the results
+if 'results' in data and len(data['results']) > 0:
+    first_result = data['results'][0]  # Access the first element of the results list
+    geometry = first_result.get('geometry')  # Get 'geometry' from the first result
+    if geometry:
+        lat = geometry.get('lat')
+        printer.pprint(lat)
+    else:
+        print("Lat data is not available in the first result.")
+else:
+    print("No results found in the response.")
 
+    ''' {'northeast': {'lat': 52.387833, 'lng': 9.7334894},
+ 'southwest': {'lat': 52.387733, 'lng': 9.7333894}}
+ '''
 
 #print(f"{name} premiered on {premiered} on {network}. It summary is {summary}")
-printer.pprint(data)
+#printer.pprint(data)
+printer.pprint(data.keys())
+
+# dict_keys(['documentation', 'licenses', 'rate', 'results', 'status', 'stay_informed', 'thanks', 'timestamp', 'total_results'])
 
 '''
 $ python3 geo_loc_test.py
