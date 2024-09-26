257c8d0ef9040cd0ac044b9c99bbd77c07328908
TK <elveli@gmail.com>
2024-09-25 17:53:46 -0700

    rewrote code

--- json/geolocation.py
+++ json/geolocation.py
@@ -5,34 +5,17 @@ import os,sys
 from opencage.geocoder import OpenCageGeocode
 from pprint import pprint
 
-value = os.getenv('PATH')
-print(value)
 
-key = 'YOUR-API-KEY'
+
+key = os.getenv('OPENCAGE_KEY')
+#print(key)
 geocoder = OpenCageGeocode(key)
-addressfile = 'addresses.txt'
+query = u'Bosutska ulica 10, Trnje, Zagreb, Croatia'
 
-try:
-  with open(addressfile,'r') as f:
-    for line in f:
-      address = line.strip()
+# no need to URI encode query, module does that for you
+results = geocoder.geocode(query)
 
-      # no need to URI encode query, module does that for you
-      results = geocoder.geocode(address, no_annotations='1')
-
-      if results and len(results):
-        longitude = results[0]['geometry']['lng']
-        latitude  = results[0]['geometry']['lat']
-        print(u'%f;%f;%s' % (latitude, longitude, address))
-        # 40.416705;-3.703582;Madrid, Spain
-        # 45.466797;9.190498;Milan, Italy
-        # 52.517037;13.388860;Berlin, Germany
-        # 48.1371079;11.5753822;München, Deutschland
-        # 52.3878553;9.7332249;Philipsbornstr 2, 30165 Hannover, Germany
-      else:
-        sys.stderr.write("not found: %s\n" % address)
-except IOError:
-  print('Error: File %s does not appear to exist.' % addressfile)
-except RateLimitExceededError as ex:
-  print(ex)
-  # You have used the requests available on your plan.
+print(u'%f;%f;%s;%s' % (results[0]['geometry']['lat'],
+                        results[0]['geometry']['lng'],
+                        results[0]['components']['country_code'],
+                        results[0]['annotations']['timezone']['name']))
