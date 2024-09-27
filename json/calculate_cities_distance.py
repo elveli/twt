d5b2901050ae0c56044951a7232776c58db24432
TK <elveli@gmail.com>
2024-09-26 20:00:03 -0700

    added city prompting

--- json/calculate_cities_distance.py
+++ json/calculate_cities_distance.py
@@ -3,7 +3,6 @@ import json,os,sys,certifi
 import requests
 import pprint
 from opencage.geocoder import OpenCageGeocode
-#from opencage.geocoder import InvalidInputError, RateLimitExceededError, UnknownError
 from pprint import PrettyPrinter
 
 printer = PrettyPrinter()
@@ -19,73 +18,43 @@ def get_latitude(results, city):
     else:
         sys.stderr.write("not found: %s\n" % city)
 
-key = os.getenv('OPENCAGE_KEY')
-#print(key)
+def main():
+    key = os.getenv('OPENCAGE_KEY')
+    #print(key)
 
-if not key:
-    print("API key is missing. Please set the OPENCAGE_KEY environment variable.")
-    exit(1)
+    if not key:
+        print("API key is missing. Please set the OPENCAGE_KEY environment variable.")
+        exit(1)
 
-#url = "http://api.opencagedata.com/geocode/v1/json?key=key&q=52.3877830%2C+9.7334394&pretty=1&no_annotations=1"
+    city1 = enter_city(1)
+    city2 = enter_city(2)
 
-url = f"http://api.opencagedata.com/geocode/v1/json?key={key}&q=52.3877830%2C+9.7334394&pretty=1&no_annotations=1"
+    geocoder = OpenCageGeocode(key)
 
-city1 = enter_city(1)
-city2 = enter_city(2)
-#print(city1, city2)
+    results_city1 = geocoder.geocode(city1, 
+                            no_annotations='1',
+                            verify=certifi.where()
+                            )
 
-geocoder = OpenCageGeocode(key)
-#results = geocoder.geocode(city1, no_annotations='1',verify=False)
-results_city1 = geocoder.geocode(city1, 
-                           no_annotations='1',
-                           verify=certifi.where()
-                        )
+    results_city2 = geocoder.geocode(city2, 
+                            no_annotations='1',
+                            verify=certifi.where()
+                            )
+    # printer.pprint(results_city1)
+    # printer.pprint(results_city2)
 
-results_city2 = geocoder.geocode(city2, 
-                           no_annotations='1',
-                           verify=certifi.where()
-                        )
-# printer.pprint(results_city1)
-# printer.pprint(results_city2)
+    lat_city1 = get_latitude(results_city1, city1)
+    lat_city2 = get_latitude(results_city2, city2)
 
-lat_city1 = get_latitude(results_city1, city1)
-lat_city2 = get_latitude(results_city2, city2)
-print(lat_city1)
-print(lat_city2)
+    each_degree_of_latitude = 111 # km
+    lat_difference = abs(lat_city1 - lat_city2) * each_degree_of_latitude
 
-lat_difference = abs(lat_city1 - lat_city2)
+    print(f"Latitude difference between {city1} and {city2} is {lat_difference:.2f} km.")
 
-print(f"Latitude difference in km between {city1} and {city2} is {lat_difference}")
-
-# if results_city1 and len(results_city1):
-#         #longitude = results[0]['geometry']['lng']
-#         city1_latitude  = results_city1[0]['geometry']['lat']
-#        # print(u'%f;%s' % (latitude, city1))
-# else:
-#         sys.stderr.write("not found: %s\n" % city1)
-
-response = requests.get(url)
-data = json.loads(response.text)
+if __name__ == "__main__":
+    main()
 
 
-# Extract the location bounds from the results
-# if 'results' in data and len(data['results']) > 0:
-#     first_result = data['results'][0]  # Access the first element of the results list
-#     geometry = first_result.get('geometry')  # Get 'geometry' from the first result
-#     if geometry:
-#         lat = geometry.get('lat')
-#         printer.pprint(lat)
-#     else:
-#         print("Lat data is not available in the first result.")
-# else:
-#     print("No results found in the response.")
-
-''' {'northeast': {'lat': 52.387833, 'lng': 9.7334894},
- 'southwest': {'lat': 52.387733, 'lng': 9.7333894}}
-'''
-
-#print(f"{name} premiered on {premiered} on {network}. It summary is {summary}")
-#printer.pprint(data)
 #printer.pprint(data.keys())
 
 # dict_keys(['documentation', 'licenses', 'rate', 'results', 'status', 'stay_informed', 'thanks', 'timestamp', 'total_results'])
